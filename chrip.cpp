5bbf1a50c018d55537d837c0130460de467663fb
henri2h <henri.carnot@hotmail.fr>
2019-10-30 18:50:14 +0100

    Load the chrip data

--- chrip.cpp
+++ chrip.cpp
@@ -1,6 +1,38 @@
 #include "chrip.h"
 
+
 Chrip::Chrip()
 {
+    // load chrip in memory
+    loadChrip();
+}
 
+void Chrip::loadChrip()
+{
+    QFile file("Data/Chirp.csv");
+       if (!file.open(QIODevice::ReadOnly)) {
+           qDebug() << file.errorString();
+           return;
+       }
+
+       file.readLine(); // remove first column
+
+       while (!file.atEnd()) {
+           QByteArray line = file.readLine();
+           QList<QByteArray> strings = line.split(',');
+
+           time.append(strings[0].toFloat());
+           amplitude.append(strings[1].toFloat());
+
+        //   qDebug() << "Word list : |" << strings[0].toFloat() << "| : |" << strings[1].toFloat() << "|";
+       }
+
+}
+
+QList<float> Chrip::getChrip(){
+    return amplitude;
+}
+
+QList<float> Chrip::getTime(){
+    return time;
 }
--- chrip.h
+++ chrip.h
@@ -1,11 +1,24 @@
 #ifndef CHRIP_H
 #define CHRIP_H
 
+#include <QFile>
+#include <QStringList>
+#include <QDebug>
+
 
 class Chrip
 {
 public:
     Chrip();
+
+    QList<float> getChrip();
+    QList<float> getTime();
+
+private:
+    void loadChrip();
+
+    QList<float> time;
+    QList<float> amplitude;
 };
 
 #endif // CHRIP_H
