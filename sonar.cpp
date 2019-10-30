ddc3b2077cd81d754252a8d3a641a613ab41c1b5
henri2h <henri.carnot@hotmail.fr>
2019-10-30 18:50:46 +0100

    Added a way to play sound and then record it

--- sonar.cpp
+++ sonar.cpp
@@ -1,6 +1,39 @@
 #include "sonar.h"
+#include <QFuture>
+#include <QtConcurrent/QtConcurrentRun>
 
 SONAR::SONAR()
 {
+    qDebug() << "Sonar started";
+  //  outputPlayer.startPlaying();
 
+    QAudioFormat format;
+    format.setSampleRate(48000);
+    format.setChannelCount(1);
+    format.setSampleSize(16);
+    format.setCodec("audio/pcm");
+    format.setByteOrder(QAudioFormat::LittleEndian);
+    format.setSampleType(QAudioFormat::SignedInt);
+
+    inputPlayer.initializeAudio(format);
+    outputPlayer.initializeAudio(format);
 }
+
+void SONAR::getDistance(){
+}
+
+
+void SONAR::startSound(){
+    auto sound =  chrip.getChrip();
+    inputPlayer.Record(2);
+    outputPlayer.Play(sound);
+}
+
+QList<float> SONAR::getResults(){
+    auto data = inputPlayer.getRecording();
+    qDebug() << "Going to play : ";
+    outputPlayer.Play(data);
+    return data;
+}
+
+
--- sonar.h
+++ sonar.h
@@ -1,11 +1,28 @@
 #ifndef SONAR_H
 #define SONAR_H
 
+#include "audioPlayer.h"
+#include "audioRecorder.h"
+
+#include "chrip.h"
+#include "threadrec.h"
 
 class SONAR
 {
 public:
     SONAR();
+    void getDistance();
+    void startSound();
+    QList<float> getResults();
+
+private:
+    AudioPlayer outputPlayer;
+    AudioRecorder inputPlayer;
+    threadrec threadRec;
+    Chrip chrip;
+
+private slots:
+    void startRecording();
 };
 
 #endif // SONAR_H
